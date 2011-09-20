90186a01811ca174b6ffe31756d2c1a703b11b17
Jonathan Dance <jd@wuputah.com>
2011-09-20 16:54:47 -0700

    remove debugging output

--- tweetscan.rb
+++ tweetscan.rb
@@ -3,8 +3,6 @@ require 'em-http'
 require 'json'
 
 creds = [ENV['TWITTER_USERNAME'], ENV['TWITTER_PASSWORD']]
-puts "Using creds: #{creds}"
-
 url = 'https://stream.twitter.com/1/statuses/filter.json'
 
 def handle_tweet(tweet)
@@ -17,10 +15,8 @@ EventMachine.run do
   http = EventMachine::HttpRequest.new(url).post(
     :body => { 'track' => 'twitter' },
     :head => { 'Authorization' => creds })
-  puts http.inspect
 
   http.stream do |chunk|
-    puts chunk.to_s
     buffer += chunk
     while line = buffer.slice!(/.+\r?\n/)
       handle_tweet JSON.parse(line)
